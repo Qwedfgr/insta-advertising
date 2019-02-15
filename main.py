d73a8e4a2bdd79726cf9231affb5e7cc74e4257a
Qwedfgr <georgiykvelidze@gmail.com>
2019-02-15 20:36:43 +0300

    Исправил наименование переменной

--- main.py
+++ main.py
@@ -63,17 +63,17 @@ def is_user_exist(bot, username):
 def get_users_tagged_friends(bot, post_id, number_of_friends_needed=2):
     comments = bot.get_media_comments_all(media_id=post_id)
     list_of_users = []
-    set_of_username = set()
+    set_of_usernames = set()
     users_exist = 0
     for comment in comments:
-        if not comment['user']['username'] in set_of_username:
+        if not comment['user']['username'] in set_of_usernames:
             users = get_users_from_comment(comment['text'])
             for user in users:
                 if is_user_exist(bot, user):
                     users_exist += 1
             if users_exist >= number_of_friends_needed:
                 list_of_users.append(comment['user'])
-                set_of_username.add(comment['user']['username'])
+                set_of_usernames.add(comment['user']['username'])
     return list_of_users
 
 
